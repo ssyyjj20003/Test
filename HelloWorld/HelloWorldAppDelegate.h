3bb2330d5e62a01eb0f436e1da21022277099654
Steven <steven@StevenAirlocal.local>
2014-06-27 09:57:44 +0800

    Initial Commit

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Steven on 14-6-27.
+//  Copyright (c) 2014年 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Steven on 14-6-27.
+//  Copyright (c) 2014年 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
